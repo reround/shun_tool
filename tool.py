b4657f8c604aea579f524ea5b26a495752024524
reround <2716753570@qq.com>
2023-06-27 10:10:46 +0800

    分离普通函数和特定类，函数

--- tool.py
+++ tool.py
@@ -4,61 +4,12 @@
 @FileName :Tek.py
 @Time :2023/6/22 8:30
 @Author :shun
-@describe: TODO
+@describe: 常用函数，类
 """
 
-import sys
 import scipy
 import numpy as np
 from constant import *
-
-
-class Tek(object):
-    """
-    Tek 示波器文件类
-    """
-
-    def __init__(self, filename) -> int:
-        self.filename = filename
-        self.parameters = self.read_parameters()
-        self.t, self.s = self.read_data()
-        self.frequency = 1 / self.parameters['Sample Interval'].astype(
-            np.float64)
-
-    def read_parameters(self) -> dict:
-        """
-        读取 tex 文件设置
-
-        :return dict: 参数和值对应的字典
-        """
-        try:
-            data = np.loadtxt(self.filename,
-                              dtype="str",
-                              delimiter=",",
-                              usecols=(0, 1),
-                              unpack=True)
-            temp = {
-                key: value
-                for key, value in zip(data[0][:18], data[1][:18]) if key != ''
-            }
-            return temp
-        except Exception as e:
-            print(e)
-
-    def read_data(self) -> tuple:
-        """
-        读取 tex 文件数据
-
-        :return tuple: 返回第3、4列，两个列表，需要两个变量接收
-        """
-        try:
-            data = np.loadtxt(self.filename,
-                              delimiter=",",
-                              usecols=(3, 4),
-                              unpack=True)
-            return (data[0], data[1])
-        except Exception as e:
-            print(e)
 
 
 class Radar(object):
